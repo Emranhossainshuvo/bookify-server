5c3a56e42d9bccb80b3252b468e2caeeba39b13a
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-02-15 07:52:56 +0600

    mongodb connection added with the mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response} from 'express'; 
 import cors from 'cors'; 
 import "dotenv/config"; 
+import mongoose from 'mongoose'; 
+
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
+
 
 const app = express(); 
 app.use(express.json()); 
@@ -13,4 +18,4 @@ app.get("/api/test", async(req: Request, res: Response) => {
 
 app.listen(7000, () => {
     console.log("server is running on local host 7000")
-})
+});
