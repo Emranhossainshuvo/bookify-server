8162f3b5f713a21456fb779c8d673dd255b26c93
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-02-17 07:07:19 +0600

    installed bcryptjs for encription and JWS for security

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -12,7 +12,12 @@ router.post("/register", async(req: Request, res: Response) => {
         if(user){
             return res.status(400).json({message: "User already exists"})
         }
-    } catch (error) {
 
+        user = new User(req.body); 
+        await user.save(); 
+
+    } catch (error) {
+        res.status(500).send({message: "Something went wrong"});
+        console.log(error); 
     }
 }); 
