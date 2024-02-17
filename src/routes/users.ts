d0b9fdcfb31be65eb9749ec4934f2d89c4e7b66e
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-02-17 13:50:47 +0600

    token function created in the users.ts file under the routes

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,6 @@
 import express, {Request, Response} from "express";
 import User from "../models/user";
+import jwt from "jsonwebtoken"
 
 const router = express.Router(); 
 
@@ -15,6 +16,8 @@ router.post("/register", async(req: Request, res: Response) => {
 
         user = new User(req.body); 
         await user.save(); 
+
+        const token = jwt.sign({userId: user.id}, process.env.JWT_SECRET_KEY as string); 
 
     } catch (error) {
         res.status(500).send({message: "Something went wrong"});
